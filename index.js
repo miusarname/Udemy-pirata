da8ef2607a77443174641552a88969fa7b4a6199
miusarname <oscarm.alvarezg@gmail.com>
2023-09-29 15:06:34 -0500

    refactor: :recycle: reafctor all code

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-console.log('a')
+// import express from "express";
+// import dotenv from "dotenv";
+// import morgan from "morgan";
+// import passport from "passport";
+// import routesVersioning from "express-routes-versioning";
+// import { limitGrt } from "./config/limiter.js";
+// import { crearToken, validarToken } from "./config/JWT.js";
+// import { reports } from "./routes/reports.routes.js";
+// import { campers } from "./routes/campers.routes.js";
+// import { Trainers } from "./routes/trainers.routes.js";
+
+// Environment variables
+dotenv.config();
+
+// Initialize server
+const index = express();
+const version = routesVersioning();
+
+// Setting
+index.set("port", process.env.PORT || 3000);
+
+// Middlewares
+index.use(morgan("dev"));
+index.use(express.json());
+index.use(limitGrt());
+index.use(passport.initialize());
+
+// Routes
+index.use("/token", crearToken);
+
+// Rutas para admin (permisos de acceso: admin)
+index.use(
+  "/admin",
+  validarToken,
+  version({
+    "3.0.0": reports,
+    fallbackVersion: "2.0.0" 
+  })
+);
+
+// Rutas para camper (permisos de acceso: camper)
+index.use(
+  "/camper",
+  validarToken,
+  version({
+    "2.0.0": campers,
+    fallbackVersion: "2.0.0" 
+  })
+);
+
+// Rutas para trainer (permisos de acceso: trainer)
+index.use(
+  "/trainer",
+  validarToken,
+  version({
+    "1.0.0": Trainers,
+    fallbackVersion: "2.0.0" 
+  })
+);
+
+// Server
+index.listen(index.get("port"), () => {
+  console.log("Server on port " + index.get("port"));
+});
